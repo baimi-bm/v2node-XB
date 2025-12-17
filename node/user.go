07029212325181fdfdd3783f6b1b8e1a4aec821c
白米 <60278829+baimi-bm@users.noreply.github.com>
2025-12-17 10:10:34 +0800

    修改v2node

---
+++ node/user.go
@@ -0,0 +1,88 @@
+package node
+
+import (
+	"strconv"
+
+	log "github.com/sirupsen/logrus"
+	panel "github.com/wyx2685/v2node/api/v2board"
+)
+
+func (c *Controller) reportUserTrafficTask() (err error) {
+	var reportmin = 0
+	var devicemin = 0
+	if c.info.Common.BaseConfig != nil {
+		reportmin = c.info.Common.BaseConfig.NodeReportMinTraffic
+		devicemin = c.info.Common.BaseConfig.DeviceOnlineMinTraffic
+	}
+	userTraffic, _ := c.server.GetUserTrafficSlice(c.tag, reportmin)
+	if len(userTraffic) > 0 {
+		err = c.apiClient.ReportUserTraffic(userTraffic)
+		if err != nil {
+			log.WithFields(log.Fields{
+				"tag": c.tag,
+				"err": err,
+			}).Info("Report user traffic failed")
+		} else {
+			log.WithField("tag", c.tag).Infof("Report %d users traffic", len(userTraffic))
+			//log.WithField("tag", c.tag).Debugf("User traffic: %+v", userTraffic)
+		}
+	}
+
+	if onlineDevice, err := c.limiter.GetOnlineDevice(); err != nil {
+		log.Print(err)
+	} else if len(*onlineDevice) > 0 {
+		var result []panel.OnlineUser
+		var nocountUID = make(map[int]struct{})
+		for _, traffic := range userTraffic {
+			total := traffic.Upload + traffic.Download
+			if total < int64(devicemin*1000) {
+				nocountUID[traffic.UID] = struct{}{}
+			}
+		}
+		for _, online := range *onlineDevice {
+			if _, ok := nocountUID[online.UID]; !ok {
+				result = append(result, online)
+			}
+		}
+		data := make(map[int][]string)
+		for _, onlineuser := range result {
+			// json structure: { UID1:["ip1","ip2"],UID2:["ip3","ip4"] }
+			data[onlineuser.UID] = append(data[onlineuser.UID], onlineuser.IP)
+		}
+		if err = c.apiClient.ReportNodeOnlineUsers(&data); err != nil {
+			log.WithFields(log.Fields{
+				"tag": c.tag,
+				"err": err,
+			}).Info("Report online users failed")
+		} else {
+			log.WithField("tag", c.tag).Infof("Total %d online users, %d Reported", len(*onlineDevice), len(result))
+			//log.WithField("tag", c.tag).Debugf("Online users: %+v", data)
+		}
+	}
+
+	userTraffic = nil
+	return nil
+}
+
+func compareUserList(old, new []panel.UserInfo) (deleted, added []panel.UserInfo) {
+	oldMap := make(map[string]int)
+	for i, user := range old {
+		key := user.Uuid + strconv.Itoa(user.SpeedLimit)
+		oldMap[key] = i
+	}
+
+	for _, user := range new {
+		key := user.Uuid + strconv.Itoa(user.SpeedLimit)
+		if _, exists := oldMap[key]; !exists {
+			added = append(added, user)
+		} else {
+			delete(oldMap, key)
+		}
+	}
+
+	for _, index := range oldMap {
+		deleted = append(deleted, old[index])
+	}
+
+	return deleted, added
+}
